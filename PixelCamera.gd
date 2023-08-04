25f849ff1c3397f37074568d8f2d30948642d71b
Moraguma <g170603@dac.unicamp.br>
2023-08-04 15:42:43 -0300

    Implements basic functionality

--- PixelCamera.gd
+++ PixelCamera.gd
@@ -17,17 +17,23 @@ func _process(delta):
 	var effective_width = width / zoom[0]
 	var effective_height = height / zoom[1]
 	
-	var top_left = Vector2(-effective_width / 2, -effective_height / 2)
-	
-	rect.position = top_left
-	rect.size = Vector2(effective_width, effective_height)
+	rect.scale = Vector2(effective_width / 1920, effective_height / 1080)
 	
 	rect.material.set_shader_parameter("zoom", zoom)
+	
+	var top_left = Vector2(-effective_width / 2, -effective_height / 2)
 	rect.material.set_shader_parameter("o", Vector2(top_left[0] / aim_resolution[0], top_left[1] / aim_resolution[1]))
 
 
 func _physics_process(delta):
-	if Input.is_action_pressed("ui_up"):
+	var dir = Vector2(Input.get_action_strength("right") - Input.get_action_strength("left"),\
+						Input.get_action_strength("down") - Input.get_action_strength("up"))
+	
+	if dir != Vector2(0, 0):
+		pass
+	position += dir * 300 * delta
+	
+	if Input.is_action_pressed("zoom"):
 		zoom += Vector2(1, 1) * delta
-	elif Input.is_action_pressed("ui_down"):
+	elif Input.is_action_pressed("unzoom"):
 		zoom -= Vector2(1, 1) * delta
